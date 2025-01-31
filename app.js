249f9eddb618d18ff4057893c38c279f3870e1dc
varun <varun.govindarajan1@gmail.com>
2025-01-31 09:52:42 +0530

    options

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ dotenv.config({ path: "./config/config.env" });
 app.use(
   cors({
     origin: [process.env.FRONTEND_URL],
-    methods: ["GET", "PUT", "DELETE", "POST"],
+    methods: ["GET", "PUT", "DELETE", "POST","OPTIONS"],
+    
     credentials: true,
   })
 );
